4cb340431feacdd85194443e73fe110191828529
Evan Taylor <taylorevan113@gmail.com>
2019-09-24 11:31:46 -0500

    Added default value for ProductMatcher in UserSettings

--- WVA_Compulink_Integration/Models/Users/UserSettings.cs
+++ WVA_Compulink_Integration/Models/Users/UserSettings.cs
@@ -10,6 +10,6 @@ namespace WVA_Connect_CDI.Models.Users
     {
         public bool DeleteBlankCompulinkOrders { get; set; } = false;
         public bool AutoFillLearnedProducts { get; set; } = true;
-        public ProductMatcher ProductMatcher { get; set; }
+        public ProductMatcher ProductMatcher { get; set; } = new ProductMatcher() { CharSequenceMaxScore = 40, SameWordMaxScore = 50, SkuTypeMaxScore = 5, QuantityMaxScore = 5 };
     }
 }
