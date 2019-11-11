7710b1145c82fa696948c8c947572b1344d387a8
Evan Taylor <taylorevan113@gmail.com>
2019-11-11 09:43:11 -0600

    Create datagrid color functionality for ImportResultsWindow

---
+++ WVA_Compulink_Integration/ViewModels/Manage/ImportResultsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WVA_Connect_CDI.Models.Manage;
+
+namespace WVA_Connect_CDI.ViewModels.Manage
+{
+    public class ImportResultsViewModel
+    {
+        public List<MatchedProductResult> MatchedProductResults = new List<MatchedProductResult>();
+
+
+
+    }
+}
--- WVA_Compulink_Integration/Views/Manage/ImportResultsWindow.xaml.cs
+++ WVA_Compulink_Integration/Views/Manage/ImportResultsWindow.xaml.cs
@@ -11,29 +11,87 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WVA_Connect_CDI.Models.Manage;
 
 namespace WVA_Connect_CDI.Views.Manage
 {
     public partial class ImportResultsWindow : Window
     {
+        //
+        // Constructors
+        //
+
         public ImportResultsWindow()
         {
             InitializeComponent();
         }
 
+        public ImportResultsWindow(List<MatchedProductResult> matchedProductResults)
+        {
+            InitializeComponent();
+            SetUpGrid(matchedProductResults);
+        }
+
+        //
+        // Initial Setup
+        //
+
+        private void SetUpGrid(List<MatchedProductResult> matchedProductResults)
+        {
+            foreach (MatchedProductResult result in matchedProductResults)
+            {
+                LearnedProductsDataGrid.Items.Add(result);
+            }
+        }
+
+
+        //
+        // UI Events
+        //
+
+
         private void RedButton_Click(object sender, RoutedEventArgs e)
         {
+            foreach (MatchedProductResult result in LearnedProductsDataGrid.Items.Cast<MatchedProductResult>())
+            {
+                if (result.RowColor == "Red")
+                    result.IsSelected = true;
+            }
 
+            LearnedProductsDataGrid.Items.Refresh();
         }
 
         private void YellowButton_Click(object sender, RoutedEventArgs e)
         {
+            foreach (MatchedProductResult result in LearnedProductsDataGrid.Items.Cast<MatchedProductResult>())
+            {
+                if (result.RowColor == "Yellow")
+                    result.IsSelected = true;
+            }
 
+            LearnedProductsDataGrid.Items.Refresh();
         }
 
         private void GreenButton_Click(object sender, RoutedEventArgs e)
         {
+            foreach (MatchedProductResult result in LearnedProductsDataGrid.Items.Cast<MatchedProductResult>())
+            {
+                if (result.RowColor == "Green")
+                    result.IsSelected = true;
+            }
+
+            LearnedProductsDataGrid.Items.Refresh();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
 
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
